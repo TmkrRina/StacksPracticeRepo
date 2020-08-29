e8b38e684293aa4fe193e71c9d8f6516fb557792
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-08-29 18:09:33 -0500

    Stacks with Array Done

--- src/com/company/ArrayStack.java
+++ src/com/company/ArrayStack.java
@@ -1,5 +1,44 @@
 package com.company;
 
-public class ArrayStack {
+import java.util.Arrays;
 
+public class ArrayStack {
+    private Integer[] stack;
+    private int count;
+
+
+    public ArrayStack(int length) {
+        stack = new Integer[length];
+    }
+
+    public boolean isEmpty() {
+        if (stack[0] == null) return true;
+        return false;
+    }
+
+    public void push(int input) {
+        Integer[] newStack = new Integer[count * 2];
+        if (stack.length == count) {
+            for (int i = 0; i < count; i++) {
+                newStack[i] = stack[i];
+
+
+            }
+            stack = newStack;
+        }
+        stack[count++] = input;
+
+    }
+
+    public int peek() {
+        return stack[count-1];
+     }
+
+     public void pop(){
+         stack[count-1] = null;
+         count--;
+         System.out.println(Arrays.toString(stack));
+
+
+     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 System.out.println("Hello world");
+	 ArrayStack stack =  new ArrayStack(5);
+	 stack.push(5);
+	 stack.push(10);
+	 stack.push(15);
+	 stack.push(20);
+	 stack.push(25);
+	 stack.push(30);
+	 stack.push(35);
+
+		stack.pop();
+		System.out.println(stack.peek());
+		System.out.println(stack.isEmpty());
+
+
     }
 }
